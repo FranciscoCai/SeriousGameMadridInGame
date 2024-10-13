d8003ec0a2ee32204bcbebc034808fb46a60a10d
JesusGalvez2 <jesusgalvezalvarez04@gmail.com>
2024-10-13 21:03:43 +0200

    Dialogue system

--- Assets/Scripts/Pruebas/DialogueScript.cs
+++ Assets/Scripts/Pruebas/DialogueScript.cs
@@ -23,7 +23,18 @@ public class DialogueScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (dialogueText.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = lines[index];
+            }
+        }
     }
 
     public void StartDialogue()
@@ -41,5 +52,19 @@ public class DialogueScript : MonoBehaviour
             yield return new WaitForSeconds(textSpeed);
         }
 
+    }
+
+    public void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            dialogueText.text = string.Empty;
+            StartCoroutine(WriteLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
