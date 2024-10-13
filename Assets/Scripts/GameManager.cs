ff4df2b07fb2fe51d3ea71aad0f6cc13fee037fc
FranciscoCai <jiehaocai54@gmail.com>
2024-10-13 23:45:09 +0200

    comprobacion

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,42 @@
+using NUnit.Framework.Constraints;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager instance;
+    public List<GameObject> ToCheck = new List<GameObject>();
+    void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        for (int i = 0; i < ToCheck.Count; i++)
+        {
+            MovebleObject toCheck = ToCheck[i].GetComponent<MovebleObject>();
+            bool correcto = toCheck.PosicionCorrecta;
+            if (correcto == true)
+            {
+                if(i == ToCheck.Count - 1)
+                {
+                    Debug.Log(1111111);
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
     }
 }
--- Assets/Scripts/MovebleObject.cs
+++ Assets/Scripts/MovebleObject.cs
@@ -1,17 +1,32 @@
+using System;
 using UnityEngine;
 
 public class MovebleObject : MonoBehaviour
 {
     [SerializeField] public Vector3 VectorRotate;
     public Vector3 upp;
+    public Vector3 vectorActual;
+    public float[] RangeFinal;
+    public bool PosicionCorrecta = false;
     void Start()
     {
-
+        if(GameManager.instance != null)
+        {
+            GameManager.instance.ToCheck.Add(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        vectorActual = gameObject.transform.rotation.eulerAngles;
+        if(vectorActual.z >= RangeFinal[0] && vectorActual.z <= RangeFinal[1])
+        {
+            PosicionCorrecta =true; 
+        }
+        else
+        {
+            PosicionCorrecta =false;
+        }
     }
 }
