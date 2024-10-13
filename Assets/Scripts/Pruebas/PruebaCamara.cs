8729d24b7241978da0475a2d341f70262aa3c889
JesusGalvez2 <jesusgalvezalvarez04@gmail.com>
2024-10-13 16:22:56 +0200

    Cámara bien hecha

--- Assets/Scripts/Pruebas/PruebaCamara.cs
+++ Assets/Scripts/Pruebas/PruebaCamara.cs
@@ -1,15 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PruebaCamara : MonoBehaviour
 {
-    public Transform target;
+    public Transform targetTransform;
+    public GameObject targetGO; 
     public float distance = 5f;
     public float targetDistance = 5f; // Distancia del objetivo para hacer zoom  
     public float zoomSpeed = 2f; 
-    public float xSpeed = 120f;
-    public float ySpeed = 120f;
+    public float xSpeed = 1000f;
+    public float ySpeed = 800f;
 
-    public float yMinLimit = -20f;
+    public float yMinLimit = -5f;
     public float yMaxLimit = 80f;
 
     // Almacenan los valores de rotación. Luego se les pasará a la cámara para que haga cosas guays :) (Girar).
@@ -28,7 +33,11 @@ public class PruebaCamara : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            targetDistance = 2f; 
+            if (targetGO == GetClickedObject(out RaycastHit hit))
+            {
+                targetDistance = 2f;
+            }
+            
         }
 
         if (Input.GetMouseButton(1))
@@ -48,7 +57,7 @@ public class PruebaCamara : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(y, x, 0);
 
         // Calcula la posición y rotación de la cámara.
-        Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
+        Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + targetTransform.position;
 
         // Aplica la rotación y posición a la cámara.
         transform.rotation = rotation;
@@ -61,5 +70,25 @@ public class PruebaCamara : MonoBehaviour
         if (angle < -360F) angle += 360F;
         if (angle > 360F) angle -= 360F;
         return Mathf.Clamp(angle, min, max);
+    }
+
+    GameObject GetClickedObject(out RaycastHit hit)
+    {
+        GameObject target = null;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            if (!isPointerOverUIObject()) { target = hit.collider.gameObject; }
+        }
+        return target;
+    }
+
+    private bool isPointerOverUIObject()
+    {
+        PointerEventData ped = new PointerEventData(EventSystem.current);
+        ped.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(ped, results);
+        return results.Count > 0;
     }
 }
