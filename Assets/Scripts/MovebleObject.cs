505a4fad6933074423a83dba0338a96e0cbb6aba
FranciscoCai <jiehaocai54@gmail.com>
2024-10-13 20:55:23 +0200

    aaaaaaaaaaaaa

--- Assets/Scripts/MovebleObject.cs
+++ Assets/Scripts/MovebleObject.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class MovebleObject : MonoBehaviour
 {
     [SerializeField] public Vector3 VectorRotate;
+    public Vector3 upp;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
