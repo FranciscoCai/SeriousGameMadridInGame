45211f37573c1b3aa3927ffa8b1b793aa2a7dcf5
JesusGalvez2 <jesusgalvezalvarez04@gmail.com>
2024-10-15 11:37:30 +0200

    Cambio de viñetas con boton

--- Assets/Scripts/Pruebas/CambiarVineta.cs
+++ Assets/Scripts/Pruebas/CambiarVineta.cs
@@ -3,10 +3,10 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 
 public class CambiarVineta : MonoBehaviour
 {
-   
 
     public Sprite[] panels;
 
@@ -15,23 +15,20 @@ public class CambiarVineta : MonoBehaviour
     public string Nivel;
 
     int index;
-
+ 
     // Update is called once per frame
     void Update()
     {
-        panelImage = GetComponent<Image>();
-
-        
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            NextPanel();
-        }
-          
+        panelImage = GetComponent<Image>(); 
     }
-
+    public void click_Start()
+    {
+        Debug.Log("Tus muertos pisaos y repisaos");
+    }
     public void NextPanel()
     {
+       
+
         if (index < panels.Length - 1)
         {
             index++;
@@ -41,5 +38,13 @@ public class CambiarVineta : MonoBehaviour
         {
             SceneManager.LoadScene(Nivel);
         }
+    }
+    public void PreviousPanel()
+    {
+        if (index > 0)
+        {
+            index--;
+            panelImage.sprite = panels[index];
+        }     
     }
 }
